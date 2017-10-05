127af1d32171c9c104792b2102d5ed82ea2c7a0e
Kyle Hendry <kylesh01@gmail.com>
2017-10-05 20:09:28 +0200

    Updated Forcebind-IP

--- ForceBindIPGui/src/GUI/Main_Frame.java
+++ ForceBindIPGui/src/GUI/Main_Frame.java
@@ -440,7 +440,7 @@ public class Main_Frame extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Metal".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
